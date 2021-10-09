2fcb9efc7368b4e95cc555cd16401a558150f1d9
= <=>
2021-10-09 14:05:19 -0300

    API Final

--- app/routes/photo.js
+++ app/routes/photo.js
@@ -1,12 +1,11 @@
 const { photoAPI } = require('../api'),
-    path = require('path'),
-    { wrapAsync, auth } = require('../infra')
+    path = require('path'), { wrapAsync, auth } = require('../infra')
 
 module.exports = app => {
 
     app.route('/:userName/photos')
         .get(wrapAsync(photoAPI.list));
-    
+
     app.route('/photos/upload')
         .post(auth, app.get('upload').single('imageFile'), wrapAsync(photoAPI.addUpload))
 
@@ -15,7 +14,7 @@ module.exports = app => {
         .delete(auth, wrapAsync(photoAPI.remove))
         .get(wrapAsync(photoAPI.findById));
 
-   
+
     app.route('/photos/:photoId/like')
         .post(auth, wrapAsync(photoAPI.like));
 };
